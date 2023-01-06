db822952e22f9d72a9788d8182c58f0703c305ba
coderscode2 <veda59@gmail.com>
2023-01-06 12:51:43 -0800

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {View, SafeAreaView, StyleSheet, Modal, TouchableHighlight } from 'react-
 import {Button, Text, Icon} from 'native-base';
 const COLS = 9;
 const ROWS = 9;
-//test2
+//test3
 class App extends Component {
   constructor(props) {
     super(props);
